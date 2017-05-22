cb277102d8dff13f6af90b8c3bca7f68b7bc8b28
LuisGonzalez <luis.rigoma@gmail.com>
2017-05-22 16:32:06 -0600

    Se esta trabajando en el crud de Usuario

---
+++ ProyectoSoft2/ProyectoSoft2/Controllers/UsuarioController.cs
@@ -0,0 +1,80 @@
+﻿using ProyectoSoft2.DB;
+using ProyectoSoft2.Models.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoSoft2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+                return View();                
+        }
+
+        [HttpGet]
+        public ActionResult ListarUsuarios()
+        {
+            using (var context = new courageproEntities())
+            {
+                var listaUsuarios = context.Usuarios.Select(x => new ListaUsuarioViewModel
+                {
+                    Id = x.IdUsuario,
+                    FirstName = x.Nombre,
+                    LastName = x.Apellido,
+                    UserName = x.AspNetUsers.UserName,
+                    BirthDate = x.FechaNacimiento,
+                    Email = x.AspNetUsers.Email,
+                    Estado = true,
+                    TipoUsuario = x.TipoUsuario.Descripcion,
+                }).ToList();
+                var jsonResult = Json(listaUsuarios, JsonRequestBehavior.AllowGet);
+                jsonResult.MaxJsonLength = Int32.MaxValue;
+                return jsonResult;
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult CrearUsuario()
+        {
+            using (var context = new courageproEntities())
+            {
+                ViewBag.ListaTipoUsuario = context.TipoUsuario.Select(x => new SelectListItem { Value = x.IdTipoUsuario.ToString(), Text = x.Descripcion });
+                 return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CrearUsuario(UsuarioViewModel model)
+        {
+            using (var context = new courageproEntities())
+            {
+ 
+                return View();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ProyectoSoft2/ProyectoSoft2/Models/Usuario/UsuarioViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoSoft2.Models.Usuario
+{
+    public class UsuarioViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo de Usuario")]
+        public int TipoUsuario { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombres")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Apellidos")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Fecha de Nacimiento")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre de Usuario")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Correo Electronico")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Contraseña")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string IdAspNetUser { get; set; }
+
+        public bool EsEditar { get; set; }
+    }
+}
